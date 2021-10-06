c994d97c8ca87915eca2770d4959a6587d3e7349
Waleson Melo <walesonmelo23@gmail.com>
2021-10-06 15:10:16 -0400

    Alterada classe Macaco
    Adicionado os metodos comer(alimento), verBucho() e digerir()

--- Questao-1/src/Macaco.java
+++ Questao-1/src/Macaco.java
@@ -1,6 +1,6 @@
 public class Macaco {
     private String nome;
-    private String bucho;
+    private String bucho = "vento";
 
     public String getNome() {
         return nome;
@@ -16,5 +16,19 @@ public class Macaco {
 
     public void setBucho(String bucho) {
         this.bucho = bucho;
+    }
+
+    // -----------------------------------------------------------------------------
+
+    public void comer(String alimento) {
+        setBucho(alimento);
+    }
+
+    public void verBucho() {
+        System.out.println("O macaco " + getNome() + " tem no bucho " + getBucho());
+    }
+
+    public void digerir() {
+        setBucho("vento");
     }
 }
