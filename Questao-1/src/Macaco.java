64cc27b1b9ef199c552ac150fd97a8fb92396096
Waleson Melo <walesonmelo23@gmail.com>
2021-10-06 15:00:32 -0400

    Alterada classe Macaco
    Atributos e metodos Get e Set criados

--- Questao-1/src/Macaco.java
+++ Questao-1/src/Macaco.java
@@ -1,2 +1,20 @@
 public class Macaco {
+    private String nome;
+    private String bucho;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getBucho() {
+        return bucho;
+    }
+
+    public void setBucho(String bucho) {
+        this.bucho = bucho;
+    }
 }
