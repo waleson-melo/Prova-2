e1d638fb963fa06b4b876f1831b746fceda3aca6
Waleson Melo <walesonmelo23@gmail.com>
2021-10-06 19:16:45 -0400

    Alterada classe ContaImposto
    Comentarios desnecessarios retirados

--- Questao-3/src/ContaImposto.java
+++ Questao-3/src/ContaImposto.java
@@ -12,11 +12,6 @@ public class ContaImposto extends ContaBancaria{
     //-------------------------------------------------------------------------------
 
     public void calcularImposto(){
-//        double saldo = getSaldo();
-//        double imposto = saldo * (getPercentualImposto() / 100);
         setSaldo(getSaldo() - (getSaldo() * (getPercentualImposto() / 100)));
-//
-//        System.out.println("Imposto: " + imposto + "Saldo: " + saldo);
-//        System.out.println("saldo depoid do desocnto: " + (saldo - imposto));
     }
 }
